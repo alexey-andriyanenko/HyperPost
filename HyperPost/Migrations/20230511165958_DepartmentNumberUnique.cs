72199aaa5860d7d2d0ee0bf0e5cbb501f6faa3c6
Alexey Andriyanenko <alexlikescoockies@gmail.com>
2023-05-11 20:01:04 +0300

    add migration script for making Deparment's Number unique

---
+++ HyperPost/Migrations/20230511165958_DepartmentNumberUnique.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HyperPost.Migrations
+{
+    /// <inheritdoc />
+    public partial class DepartmentNumberUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Department_Number",
+                table: "Department",
+                column: "Number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Department_Number",
+                table: "Department");
+        }
+    }
+}
--- HyperPost/Models/DepartmentModel.cs
+++ HyperPost/Models/DepartmentModel.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HyperPost.Models
 {
     [Table("Department")]
+    [Index(nameof(Number), IsUnique = true)]
     public class DepartmentModel
     {
         public int Id { get; set; }
