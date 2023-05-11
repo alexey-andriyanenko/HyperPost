f2ef458bd8e5013fa26bd4851393e9c95f74d17c
Alexey Andriyanenko <alexlikescoockies@gmail.com>
2023-05-11 19:39:05 +0300

    add updated InitialSeed migration (with Departments)

--- HyperPost/DB/HyperPostDbContext.cs
+++ HyperPost/DB/HyperPostDbContext.cs
@@ -12,5 +12,138 @@ namespace HyperPost.DB
         public DbSet<PackageCategoryModel> PackageCategoties { get; set; }
         public DbSet<PackageStatusModel> PackageStatuses { get; set; }
         public HyperPostDbContext(DbContextOptions<HyperPostDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserModel>()
+                .HasData(
+                    new UserModel
+                    {
+                        Id = 1,
+                        RoleId = 1,
+                        FirstName = "Admin",
+                        LastName = "User",
+                        PhoneNumber = "111111",
+                        Email = "admin@example.com",
+                        Password = "root",
+                    },
+                    new UserModel
+                    {
+                        Id = 2,
+                        RoleId = 2,
+                        FirstName = "Manager",
+                        LastName = "User",
+                        PhoneNumber = "222222",
+                        Email = "manager@example.com",
+                        Password = "manager_password",
+                    },
+                    new UserModel
+                    {
+                        Id = 3,
+                        RoleId = 3,
+                        FirstName = "Client",
+                        LastName = "User",
+                        PhoneNumber = "333333",
+                        Email = "client@example.com",
+                        Password = "client_password",
+                    }
+                );
+
+            modelBuilder.Entity<UserRoleModel>()
+                .HasData(
+                    new UserRoleModel { Id = 1, Name = "admin" },
+                    new UserRoleModel { Id = 2, Name = "manager" },
+                    new UserRoleModel { Id = 3, Name = "client" }
+                );
+
+            modelBuilder.Entity<PackageStatusModel>()
+                .HasData(
+                    new PackageStatusModel { Id = 1, Name = "created" },
+                    new PackageStatusModel { Id = 2, Name = "sent" },
+                    new PackageStatusModel { Id = 3, Name = "arrived" },
+                    new PackageStatusModel { Id = 4, Name = "received" }
+                );
+
+            modelBuilder.Entity<PackageCategoryModel>()
+                .HasData(
+                    new PackageCategoryModel { Id = 1, Name = "Food" },
+                    new PackageCategoryModel { Id = 2, Name = "Money" },
+                    new PackageCategoryModel { Id = 3, Name = "Medicaments" },
+                    new PackageCategoryModel { Id = 4, Name = "Accumulators" },
+                    new PackageCategoryModel { Id = 5, Name = "Sports Products" },
+                    new PackageCategoryModel { Id = 6, Name = "Clothes" },
+                    new PackageCategoryModel { Id = 7, Name = "Shoes" },
+                    new PackageCategoryModel { Id = 8, Name = "Documents" },
+                    new PackageCategoryModel { Id = 9, Name = "Books" },
+                    new PackageCategoryModel { Id = 10, Name = "Computers" },
+                    new PackageCategoryModel { Id = 11, Name = "Accessories" }
+                );
+
+            modelBuilder.Entity<DepartmentModel>()
+                .HasData(
+                    new DepartmentModel
+                    {
+                        Id = 1,
+                        Number = 1,
+                        FullAddress = "HyperPost Department #1, 5331 Rexford Court, Montgomery AL 36116"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 2,
+                        Number = 2,
+                        FullAddress = "HyperPost Department #2, 6095 Terry Lane, Golden CO 80403"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 3,
+                        Number = 3,
+                        FullAddress = "HyperPost Department #3, 1002 Hilltop Drive, Dalton GA 30720"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 4,
+                        Number = 4,
+                        FullAddress = "HyperPost Department #4, 2325 Eastridge Circle, Moore OK 73160"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 5,
+                        Number = 5,
+                        FullAddress = "HyperPost Department #5, 100219141 Pine Ridge Circle, Anchorage AK 99516"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 6,
+                        Number = 6,
+                        FullAddress = "HyperPost Department #6, 5275 North 59th Avenue, Glendale AZ 85301"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 7,
+                        Number = 7,
+                        FullAddress = "HyperPost Department #7, 5985 Lamar Street, Arvada CO 80003"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 8,
+                        Number = 8,
+                        FullAddress = "HyperPost Department #8, 136 Acacia Drive, Blue Lake CA 95525"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 9,
+                        Number = 9,
+                        FullAddress = "HyperPost Department #9, 7701 Taylor Oaks Circle, Montgomery AL 36116"
+                    },
+                    new DepartmentModel
+                    {
+                        Id = 10,
+                        Number = 10,
+                        FullAddress = "HyperPost Department #10, 243 Kentucky Avenue, Pasadena MD 21122"
+                    }
+                );
+        }
     }
 }
---
+++ HyperPost/Migrations/20230511163754_InitialSeed.cs
@@ -0,0 +1,241 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HyperPost.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Department",
+                columns: new[] { "Id", "FullAddress", "Number" },
+                values: new object[,]
+                {
+                    { 1, "HyperPost Department #1, 5331 Rexford Court, Montgomery AL 36116", 1 },
+                    { 2, "HyperPost Department #2, 6095 Terry Lane, Golden CO 80403", 2 },
+                    { 3, "HyperPost Department #3, 1002 Hilltop Drive, Dalton GA 30720", 3 },
+                    { 4, "HyperPost Department #4, 2325 Eastridge Circle, Moore OK 73160", 4 },
+                    { 5, "HyperPost Department #5, 100219141 Pine Ridge Circle, Anchorage AK 99516", 5 },
+                    { 6, "HyperPost Department #6, 5275 North 59th Avenue, Glendale AZ 85301", 6 },
+                    { 7, "HyperPost Department #7, 5985 Lamar Street, Arvada CO 80003", 7 },
+                    { 8, "HyperPost Department #8, 136 Acacia Drive, Blue Lake CA 95525", 8 },
+                    { 9, "HyperPost Department #9, 7701 Taylor Oaks Circle, Montgomery AL 36116", 9 },
+                    { 10, "HyperPost Department #10, 243 Kentucky Avenue, Pasadena MD 21122", 10 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PackageCategory",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Food" },
+                    { 2, "Money" },
+                    { 3, "Medicaments" },
+                    { 4, "Accumulators" },
+                    { 5, "Sports Products" },
+                    { 6, "Clothes" },
+                    { 7, "Shoes" },
+                    { 8, "Documents" },
+                    { 9, "Books" },
+                    { 10, "Computers" },
+                    { 11, "Accessories" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "PackageStatus",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "created" },
+                    { 2, "sent" },
+                    { 3, "arrived" },
+                    { 4, "received" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "User",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "Password", "PhoneNumber", "RoleId" },
+                values: new object[,]
+                {
+                    { 1, "admin@example.com", "Admin", "User", "root", "111111", 1 },
+                    { 2, "manager@example.com", "Manager", "User", "manager_password", "222222", 2 },
+                    { 3, "client@example.com", "Client", "User", "client_password", "333333", 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "admin" },
+                    { 2, "manager" },
+                    { 3, "client" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Department",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "PackageCategory",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "PackageStatus",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PackageStatus",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PackageStatus",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PackageStatus",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
